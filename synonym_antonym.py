e1a335660c377460e7b0db3360ed552476901228
hanif-sudoapt <amrhanif15@gmail.com>
2021-01-10 13:43:40 +0700

    add a new comment and make the code more cleaner, and then change i variable become answer variable in the for loop

--- synonym_antonym.py
+++ synonym_antonym.py
@@ -1,18 +1,21 @@
 # If you want to copy this code, make sure that you have install the PyDictionary module
 # Install ---> pip install PyDictionary
 
+# import the PyDictionary module
 from PyDictionary import PyDictionary
+
+# make the instance
 dictionary = PyDictionary()
 
+# variable:
+line = '=================================================================================================='
+
 def welcome(name):
-    print('======================================================================================================')
-    print(f'Welcome {name}, this program was made for people who want to search an antonym or synonym from a word')
-    print('======================================================================================================')
-    print('*warning, you just can input one word! not more')
+    # I have make this function a lot cleaner, than before
+    print(f"{line}\nWelcome {name}, this program was made for people who want to search an antonym or synonym of a word\n{line}\n*warning, you just can input one word! not more")
 
 def choice():
-    print()
-    choice_types = input('What do you want to search from a word?\n1. Synonyms\n2. Antonyms\n3. Exit\njust choose the number: ')
+    choice_types = input('\nWhat do you want to search from a word?\n1. Synonyms\n2. Antonyms\n3. Exit\njust choose the number: ')
     print()
     if choice_types == '1':
         synonym()
@@ -27,21 +30,25 @@ def synonym():
     word = input('Input the word that you want to search its synonym: ')
     result = dictionary.synonym(word)
     print(f'So the synonym of {word} is:')
-    for i in result:
-        print('-', i)
+    # I'm using for in loop, for looping the answer. It will make it more tidy and cleaner to look
+    for answer in result:
+        print('-', answer)
     choice()
+    # back to the choice function
 
 def antonym():
     word = input('Input the word that you want to search its antonym: ')
     result = dictionary.antonym(word)
     print(f'So the antonym of {word} is:')
-    for i in result:
-        print('-', i)
+    # I'm using for in loop, for looping the answer. Because, it will make it more tidy and cleaner to look
+    for answer in result:
+        print('-', answer)
     choice()
+    # back to the choice function
 
 def exit_out():
     print("Thanks for using this program :)\n")
-
+    # program get close
 
 name = input('Type in your name: ')
 
@@ -49,5 +56,4 @@ welcome(name)
 choice()
 
 
-    
-
+# commit = "add a new comment and make the code more cleaner, and then change i variable become answer variable in the for loop"
