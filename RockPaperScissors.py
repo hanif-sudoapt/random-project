ea4c5e17b696712f5b9e79c65551efd374eb26e6
hanif-sudoapt <amrhanif15@gmail.com>
2021-01-18 08:17:10 +0700

    print a string when the game is get quit, make a name input for the user, and edit some line

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,27 +1,28 @@
 import random
 
+# this is a new feature, that make user can type in their name 
+name = input("Enter your name: ")
+
 # all bunch of variables is stored in here :)
 
 game_list = ['Rock', 'Paper', 'Scissors'] # this is the answer choice 
 computer_score = 0 # this is the variable of computer score
 player_score = 0 # this is the variable of player score
-player_won = "\nPlayer Won!" # this is the string that we want to print when the player won from the computer
+player_won = f"\n{name} Won!" # this is the string that we want to print when the player won from the computer
 computer_won = "\nComputer Won!" # this is the string that we want to print when the computer won from the player
-tie = "Tie !!"
+tie = "\nTie !!"
 
 # just a line, for separate line by line  
-line = "================================"
-second_line = "--------------------------------"
+line = "======================================"
+second_line = "--------------------------------------"
 
 # the loop
 run = True
 
 while run:
-    print(line)
-    print(f"Score: Computer = {computer_score}, Player = {player_score}")
+    print(f"{line}\nMain Score: Computer = {computer_score}, {name} = {player_score}\n{second_line}")
     computer_choice = random.choice(game_list)
-    print(second_line)
-    player_choice = input("Rock, Paper, Scissors, or Quit: ").capitalize()
+    player_choice = input("Rock, Paper, Scissors, or Quit: ").capitalize() 
     if player_choice == computer_choice:
         print(tie)
 
@@ -53,12 +54,14 @@ while run:
             computer_score += 1
 
     elif player_choice == "Quit":
+        print("\nBye .. Bye ..\n")
         break
 
     else:
         print("\nInvalid Input !\n")
         player_choice = "(Input Error)"
     
-    print(f"- Player: {player_choice}")
-    print(f"- Computer: {computer_choice}")
-    print()
+    print(f"- {name}: {player_choice}")
+    print(f"- Computer: {computer_choice}\n")
+
+# "print a string when the game is get quit, make a name input for the user, and edit some line"
