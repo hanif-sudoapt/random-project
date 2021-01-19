920c50f58336ca973352c0bd91681689f44ddaec
hanif-sudoapt <amrhanif15@gmail.com>
2021-01-19 19:48:16 +0700

    ada bug, yaitu score sementara tidak bisa bertambah. Ini terjadi setelah adanya fitur baru yang dibuat. Fiturnya adalah garis yang membatasi skor sementara, dapat menyesuaikan panjangnya dengan panjang kalimat skor sementara.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,26 +1,29 @@
 import random
 
-# this is a new feature, that make user can type in their name 
+# the player, can input their own name 
 name = input("Enter your name: ")
 
 # all bunch of variables is stored in here :)
-
-game_list = ['Rock', 'Paper', 'Scissors'] # this is the answer choice 
+game_list = ['Rock', 'Paper', 'Scissors'] # this is the answer choice
 computer_score = 0 # this is the variable of computer score
 player_score = 0 # this is the variable of player score
 player_won = f"\n{name} Won!" # this is the string that we want to print when the player won from the computer
 computer_won = "\nComputer Won!" # this is the string that we want to print when the computer won from the player
-tie = "\nTie !!"
+tie = "\nTie Game !!"
 
-# just a line, for separate line by line  
-line = "======================================"
-second_line = "--------------------------------------"
+main_score = f"Main Score: Computer = {computer_score}, {name} = {player_score}" # this is a variable for printing the main score
+main_score_count = len(main_score) # this is for counting the length of the main_score variable
+
+# just a line, for separating line by line (make the output looks more cleaner)  
+upper_line = "=" * main_score_count # so, I times the line with the main score length, because it makes the line looks more efficient
+down_line = "-" * main_score_count 
 
 # the loop
 run = True
 
 while run:
-    print(f"{line}\nMain Score: Computer = {computer_score}, {name} = {player_score}\n{second_line}")
+
+    print(f"{upper_line}\n{main_score}\n{down_line}")
     computer_choice = random.choice(game_list)
     player_choice = input("Rock, Paper, Scissors, or Quit: ").capitalize() 
     if player_choice == computer_choice:
