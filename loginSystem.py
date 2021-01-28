abde69fcb2d82fa7ae50e3d589fcc84eb16bc07c
hanif-sudoapt <amrhanif15@gmail.com>
2021-01-28 14:18:33 +0700

    menambahkan logika lebih saat login

--- loginSystem.py
+++ loginSystem.py
@@ -1,25 +1,57 @@
+# Komentar Berbahasa Indonesia !
+
+# ini dictionary untuk menyimpan data akun user
 user_data = {}
 
 def welcome():
-    choice = input("Welcome\n1. Sign Up\n2. Login\n3. Exit\n\n")
+    choice = input("\nWelcome\n1. Sign Up\n2. Login\n3. Exit\n== ")
     if choice == "1":
+        # masuk ke fungsi sign_up
         sign_up()
     elif choice == "2":
+        # masuk ke fungsi login
         login()
+    else:
+        pass
+        # keluar dari program, jika user input angka untuk exit
 
     
 def sign_up():
-    # sign up for new user account
+    # sign up untuk akun baru
     new_username = input("Input new username: ")
-    new_password = int(input("Input new password: "))
+    new_password = input("Input new password: ")
+
     # store the data to the dictionary
     user_data[new_username] = new_password
+
+    # kembali ke fungsi welcome
     welcome()
-    # back to the welcome function
 
 def login():
+    # meminta input untuk login user
     username = input("Enter your username: ")
-    password = int(input("Enter your password: "))
-    # stop from here
+    password = input("Enter your password: ")
 
-# This program is unfinished yet
+    # logika untuk cek keberadaan akun
+    if username in user_data.keys():
+        if password in user_data.values():
+            print("\n*Login successfully")
+
+            # kembali ke fungsi welcome
+            welcome()
+        else:
+            print("\n*This account is doesn't exist")
+
+            # kembali ke fungsi welcome
+            welcome()
+    else:
+        print("\n*This account is doesn't exist")
+
+        # kembali ke fungsi welcome
+        welcome()
+
+# start program
+welcome()
+
+
+# Program ini belum selesai tuntas, masih banyak yang bisa di improve kedepannya
