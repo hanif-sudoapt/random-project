027850d1703ae75ad79b6357bf4d82b74c5370cd
hanif-sudoapt <amrhanif15@gmail.com>
2021-01-28 15:14:38 +0700

    menambahkan beberapa fitur baru, dan menambahkan banyak comment untuk memperjelas cara kerja program

--- loginSystem.py
+++ loginSystem.py
@@ -3,17 +3,24 @@
 # ini dictionary untuk menyimpan data akun user
 user_data = {}
 
+# ini password rahasia hehe
+secret_password = "kopinya enak banget"
+
 def welcome():
-    choice = input("\nWelcome\n1. Sign Up\n2. Login\n3. Exit\n== ")
+    choice = input("\nWelcome\n1. Sign Up\n2. Login\n3. All Data\n(press enter for exit)\n--> ")
+    print()
     if choice == "1":
         # masuk ke fungsi sign_up
         sign_up()
     elif choice == "2":
         # masuk ke fungsi login
         login()
+    elif choice == "3":
+        # masuk ke fungsi all_data
+        all_data()
     else:
         pass
-        # keluar dari program, jika user input angka untuk exit
+        # keluar dari program, jika user menekan enter :)
 
     
 def sign_up():
@@ -21,11 +28,24 @@ def sign_up():
     new_username = input("Input new username: ")
     new_password = input("Input new password: ")
 
-    # store the data to the dictionary
-    user_data[new_username] = new_password
+    # algoritma untuk cek apakah ada spasi di dalam username ataupun password
+    if " " in new_username:
+        print("\n*No space allowed in username")
+    elif " " in new_password:
+        print("\n*No space allowed in password")
+    # jika sudah benar tidak ada spasi pada username maupun password, maka username dan password dapat dilanjut ke tahap berikutnya
+    else:
+        # jika username sudah pernah di input sebelumnya, maka username tidak akan bisa diterima komputer
+        if new_username in user_data.keys():
+            print("\n*This account is already exist")
+        else:
+            # mentransfer data ke dictionary "user_data"
+            user_data[new_username] = new_password
+            print("\n*Account added")
 
     # kembali ke fungsi welcome
     welcome()
+
 
 def login():
     # meminta input untuk login user
@@ -36,19 +56,28 @@ def login():
     if username in user_data.keys():
         if password in user_data.values():
             print("\n*Login successfully")
-
-            # kembali ke fungsi welcome
-            welcome()
         else:
             print("\n*This account is doesn't exist")
-
-            # kembali ke fungsi welcome
-            welcome()
     else:
         print("\n*This account is doesn't exist")
 
-        # kembali ke fungsi welcome
-        welcome()
+    # kembali ke fungsi welcome
+    welcome()
+
+
+def all_data():
+    # di sini, kita bisa akses semua data yang telah tersimpan di dalam dictionary "user_data"
+    secret_input = input("Enter the secret password: ")
+    if secret_input == secret_password:
+        print("\nUser Data:")
+        for username, password in user_data.items():
+            print(f"- {username} == {password}")
+    else:
+        print("\n*Invalid password")
+
+    # kembali ke fungsi welcome
+    welcome()
+
 
 # start program
 welcome()
